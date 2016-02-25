0d59e00efac7be8b08dada434a11da5e8cc1abb6
태섭 <@tslim-pc>
2016-02-25 13:29:14 +0900

    초기화 루틴 추가

---
+++ ProWeb/src/main/java/iedu/util/MailSendInfo.java
@@ -0,0 +1,17 @@
+package iedu.util;
+
+public class MailSendInfo {
+
+   public int port;
+   public String from;
+   public String fromname;
+   public String to;
+   public String smtpserver;   
+   public String userid;
+   public String password;
+   public  boolean istls = false;
+   public  boolean isssl = false;
+   public String subject;
+   public String body;
+
+}
