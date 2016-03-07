4fa368fb014dc6efcd24c1591db72a6fde3bb87e
이호진 <ssyoui@naver.com>
2016-03-07 10:21:20 +0900

    Search.. (테스트)

---
+++ ProWeb/src/main/java/com/wing/mainApp/controller/SearchController.java
@@ -0,0 +1,18 @@
+package com.wing.mainApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SearchController {
+	
+	/*@Autowired*/
+	
+	@RequestMapping("/Search/SearchForm")
+	public ModelAndView SearchForm(){
+		
+		return null;
+	}
+
+}
