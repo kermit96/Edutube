e334fe6da02caf74a3ad34249ca6fef233ed1372
이호진 <ssyoui@naver.com>
2016-03-07 10:39:16 +0900

    복구

--- ProWeb/src/main/java/com/wing/mainApp/sql/DBTYPE.java
+++
@@ -1,7 +0,0 @@
-package com.wing.mainApp.sql;
-public enum DBTYPE {
-	ORACLE_TYPE,MSSQL_TYPE,MYSQL_TYPE;
-    public static DBTYPE fromInt(int i) {    	        	
-        	return DBTYPE.values()[i];   
-    }
-}
