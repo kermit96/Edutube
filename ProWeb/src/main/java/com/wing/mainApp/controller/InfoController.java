e9701d5c054ddb72489f3cae762f8c3cf1e8dc3b
태섭 <@tslim-pc>
2016-03-18 16:31:57 +0900

    info 에러 수정

--- ProWeb/src/main/java/com/wing/mainApp/controller/InfoController.java
+++ ProWeb/src/main/java/com/wing/mainApp/controller/InfoController.java
@@ -10,32 +10,32 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class InfoController {
 	
-	@RequestMapping("Info/InfoMain.do")
-	public ModelAndView infomain(){
+	@RequestMapping("/Info/InfoMain")
+	public ModelAndView infomain(HttpServletRequest req, HttpSession session){
 		ModelAndView 	mv = new ModelAndView();
-		
-		mv.setViewName("Info/Infomain");
+
+		mv.setViewName("/Info/InfoMain");
 		return mv;
 	}
-	@RequestMapping("/Info/info.do")
+	@RequestMapping("/Info/info")
 	public ModelAndView info(HttpServletRequest req, HttpSession session){
 		ModelAndView 	mv = new ModelAndView();
 		
-		mv.setViewName("Info/info");
+		mv.setViewName("/Info/info");
 		return mv;
 	}
-	@RequestMapping("/Info/map.do")
+	@RequestMapping("/Info/Map")
 	public ModelAndView map(HttpServletRequest req, HttpSession session){
 		ModelAndView 	mv = new ModelAndView();
 		
-		mv.setViewName("Info/Map");
+		mv.setViewName("/Info/Map");
 		return mv;
 	}
-	@RequestMapping("/Info/use.do")
+	@RequestMapping("/Info/Use")
 	public ModelAndView use(HttpServletRequest req, HttpSession session){
 		ModelAndView 	mv = new ModelAndView();
 		
-		mv.setViewName("Info/Use");
+		mv.setViewName("/Info/Use");
 		return mv;
 	}
 	
