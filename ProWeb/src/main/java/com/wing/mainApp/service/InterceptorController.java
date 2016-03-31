2bacd357b423f6d871a206e820dedd670d95b986
태섭 <@tslim-pc>
2016-03-31 14:36:26 +0900

    db 변경

--- ProWeb/src/main/java/com/wing/mainApp/service/InterceptorController.java
+++ ProWeb/src/main/java/com/wing/mainApp/service/InterceptorController.java
@@ -19,53 +19,37 @@ public class InterceptorController extends HandlerInterceptorAdapter  {
 	@Override
 		public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 				throws Exception {
-			// TODO Auto-generated method stub
-				
-	
-		
-		  HttpSession ses =  request.getSession();
-		  
+			// TODO Auto-generated method stub						
+		  HttpSession ses =  request.getSession();		  
 		  String lang = (String)ses.getAttribute("LANG");
 		  
 		  if (StringUtil.isNull(lang)) {
 
-			  Cookie[] cookes = request.getCookies();
-			  
+			  Cookie[] cookes = request.getCookies();			  
 			// cookes 에서 값을 가졍 온다 .
 			  // 값이 없으면   lang 을 ko 로 하고  cookie 에 넣은다 .
 			  
-			  if(cookes != null ) {
- 			  
+			  if(cookes != null ) { 			  
 	              for(Cookie cookie :cookes) {
 	            	  if (cookie.getName().equals("LANG"))
 	            	  {            		  
 	            		  lang = cookie.getValue();
 	            		  break;            		  
 	            	  }            	  
-	                }
-              
+	                }              
 			  }
 			  
               if (StringUtil.isNull(lang)) {
             	  lang = "ko";
             	  response.addCookie(new Cookie("LANG",lang));             	  
-              }
-              
-              ses.setAttribute("LANG", lang);
-                                          			  
+              }              
+              ses.setAttribute("LANG", lang);                                          			  
 		  }
 		  
-		  
- 
-		  
-		   HashMap map = Langxml.load(lang);    
-	
-		    request.setAttribute("LANGDATA", map);
-		  		  
-		  return super.preHandle(request, response, handler);
-		  
+		   HashMap map = Langxml.load(lang);    	
+		    request.setAttribute("LANGDATA", map);		  		  
+		  return super.preHandle(request, response, handler);	  
 		}
 
-	
 	
 }
